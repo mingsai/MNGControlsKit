50ab0b142a8e510a215165a31a28b05c94cc3fdc
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-09-30 12:03:40 -0400

    Initial Commit

---
+++ MNGControlsKit/MNGControlsKit.swift
@@ -0,0 +1,11 @@
+//
+//  MNGControlsKit.swift
+//  MNGControlsKit
+//
+//  Created by Tommie Carter on 9/30/19.
+//  Copyright © 2019 MING Technology LLC. All rights reserved.
+//
+
+class MNGControlsKit {
+
+}
